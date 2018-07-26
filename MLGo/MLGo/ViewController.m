1b39d7b18c779f71ec223d935cb15e3f3caa4aba
Maple <huidragonaijy@gmail.com>
2018-07-26 08:53:46 +0800

    Chore: init project by cocoapods

---
+++ MLGo/MLGo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  MLGo
+//
+//  Created by Maple on 2018/7/26.
+//  Copyright © 2018 Maple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UITabBarController
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
